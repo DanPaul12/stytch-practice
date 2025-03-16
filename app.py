aed58304ff1f0c9998e69f78563d97f1a50d563c
DanPaul12 <dan.paul.schechter@gmail.com>
2025-03-16 16:54:39 -0700

    -

--- app.py
+++ app.py
@@ -3,26 +3,24 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-project = os.getenv('STYTCH_PROJECT_ID')
-secret = os.getenv('STYTCH_SECRET')
-url = "https://test.stytch.com/v1/magic_links/email/send"
+PROJECT_ID = os.getenv('STYTCH_PROJECT_ID')
+SECRET = os.getenv('SYTCH_SECRET')
 
-headers = {
-    "Content-Type": "application/json"
-}
+STYTCH_API_URL = 'https://test.stytch.com/v1/magic_links/email/send'
 
-payload = {
-    "email": "dan.paul.schechter@gmail.com",
-    "login_magic_link_url": "https://yourapp.com/auth/callback",
-    "signup_magic_link_url": "https://yourapp.com/signup/callback",
-}
+headers = {'Content-Type':'application/json'}
 
-auth = (
-    project,
-    secret
-)
+auth = (PROJECT_ID, SECRET)
 
-response = requests.post(url, json=payload, headers=headers, auth=auth)
-response.raise_for_status()
-print(response.status_code)
-print(response.text)
+payload = {'email':'dan.paul.schechter@gmail.com'}
+
+try:
+    response = requests.post(url=STYTCH_API_URL, auth=auth, headers=headers, json=payload)
+    response.raise_for_status()
+    if response.status_code == 200:
+        print('email sent!')
+    else:
+        print(f'Error code: {response.status_code}')
+        print(response.text)
+except requests.exceptions.RequestException as e:
+    print(f'Unexpected error: {e}')
